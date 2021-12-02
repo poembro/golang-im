2b949bcc47ec6bed8276f016cba7c425f1125213
luoyuxiang <luoyuxiang@luoyuxiangdeMacBook-Pro.local>
2021-12-02 16:22:10 +0800

    add rpc api

--- pkg/grpclib/etcdv3/register.go
+++ pkg/grpclib/etcdv3/register.go
@@ -1,113 +1,104 @@
 package etcdv3
 
 import (
-    "context"
-    "fmt"
-    "net"
-    "strconv"
-    "strings"
+	"context"
+	"fmt"
+	"net"
+	"strings"
 
-    "go.etcd.io/etcd/clientv3"
+	"go.etcd.io/etcd/clientv3"
 )
 
 type RegEtcd struct {
-    cli    *clientv3.Client
-    ctx    context.Context
-    cancel context.CancelFunc
-    key    string
+	cli    *clientv3.Client
+	ctx    context.Context
+	cancel context.CancelFunc
+	key    string
 }
 
 var rEtcd *RegEtcd
 
 // "%s:///%s/"
 func GetPrefix(schema, serviceName string) string {
-    return fmt.Sprintf("%s:///%s/", schema, serviceName)
+	return fmt.Sprintf("%s:///%s/", schema, serviceName)
 }
 
 // "%s:///%s"
 func GetPrefix4Unique(schema, serviceName string) string {
-    return fmt.Sprintf("%s:///%s", schema, serviceName)
+	return fmt.Sprintf("%s:///%s", schema, serviceName)
 }
 
 // "%s:///%s/" ->  "%s:///%s:ip:port"
-func RegisterEtcd4Unique(schema, etcdAddr, myHost string, myPort int, serviceName string, ttl int) error {
-    serviceName = serviceName + ":" + net.JoinHostPort(myHost, strconv.Itoa(myPort))
-    return RegisterEtcd(schema, etcdAddr, myHost, myPort, serviceName, ttl)
+func RegisterEtcd4Unique(schema, etcdAddr, myHost string, myPort string, serviceName string, ttl int) error {
+	serviceName = serviceName + ":" + net.JoinHostPort(myHost, myPort)
+	return RegisterEtcd(schema, etcdAddr, myHost, myPort, serviceName, ttl)
 }
 
 func Register(schema, etcdAddr, ipPort, serviceName string, ttl int) error {
-    if strings.Index(ipPort, ":") == -1 {
-        return fmt.Errorf("localhost addr and port format error ")
-    }
-    arr := strings.Split(ipPort, ":")
-    if len(arr) != 2 {
-        return fmt.Errorf("strings.Split(ipPort, ':')  " + ipPort)
-    }
-    ip := arr[0]
-    port, err := strconv.Atoi(arr[1])
-    if err != nil {
-        return fmt.Errorf("port not int error ")
-    }
-    // TODO 可能更多 服务注册与发现 的持久化工具
-    return RegisterEtcd(schema, etcdAddr, ip, port, serviceName, ttl)
+	host, port, err := net.SplitHostPort(ipPort)
+	if err != nil {
+		return fmt.Errorf("port not int error ")
+	}
+	// TODO 可能更多 服务注册与发现 的持久化工具
+	return RegisterEtcd(schema, etcdAddr, host, port, serviceName, ttl)
 }
 
 //etcdAddr separated by commas  注册服务
-func RegisterEtcd(schema, etcdAddr, myHost string, myPort int, serviceName string, ttl int) error {
-    cli, err := clientv3.New(clientv3.Config{
-        Endpoints: strings.Split(etcdAddr, ","),
-    })
-    fmt.Println("RegisterEtcd")
-    if err != nil {
-        //        return fmt.Errorf("grpclb: create clientv3 client failed: %v", err)
-        return fmt.Errorf("create etcd clientv3 client failed, errmsg:%v, etcd addr:%s", err, etcdAddr)
-    }
+func RegisterEtcd(schema, etcdAddr, myHost string, myPort string, serviceName string, ttl int) error {
+	cli, err := clientv3.New(clientv3.Config{
+		Endpoints: strings.Split(etcdAddr, ","),
+	})
+	fmt.Println("etcd Register")
+	if err != nil {
+		//        return fmt.Errorf("grpclb: create clientv3 client failed: %v", err)
+		return fmt.Errorf("create etcd clientv3 client failed, errmsg:%v, etcd addr:%s", err, etcdAddr)
+	}
 
-    //lease
-    ctx, cancel := context.WithCancel(context.Background())
-    resp, err := cli.Grant(ctx, int64(ttl))
-    if err != nil {
-        return fmt.Errorf("grant failed")
-    }
+	//lease
+	ctx, cancel := context.WithCancel(context.Background())
+	resp, err := cli.Grant(ctx, int64(ttl))
+	if err != nil {
+		return fmt.Errorf("grant failed")
+	}
 
-    //  schema:///serviceName/ip:port ->ip:port
-    serviceValue := net.JoinHostPort(myHost, strconv.Itoa(myPort))
-    serviceKey := GetPrefix(schema, serviceName) + serviceValue
+	//  schema:///serviceName/ip:port ->ip:port
+	serviceValue := net.JoinHostPort(myHost, myPort)
+	serviceKey := GetPrefix(schema, serviceName) + serviceValue
 
-    //set key->value
-    if _, err := cli.Put(ctx, serviceKey, serviceValue, clientv3.WithLease(resp.ID)); err != nil {
-        return fmt.Errorf("put failed, errmsg:%v， key:%s, value:%s", err, serviceKey, serviceValue)
-    }
+	//set key->value
+	if _, err := cli.Put(ctx, serviceKey, serviceValue, clientv3.WithLease(resp.ID)); err != nil {
+		return fmt.Errorf("put failed, errmsg:%v， key:%s, value:%s", err, serviceKey, serviceValue)
+	}
 
-    //keepalive
-    kresp, err := cli.KeepAlive(ctx, resp.ID)
-    if err != nil {
-        return fmt.Errorf("keepalive faild, errmsg:%v, lease id:%d", err, resp.ID)
-    }
+	//keepalive
+	kresp, err := cli.KeepAlive(ctx, resp.ID)
+	if err != nil {
+		return fmt.Errorf("keepalive faild, errmsg:%v, lease id:%d", err, resp.ID)
+	}
 
-    go func() {
-    FLOOP:
-        for {
-            select {
-            case _, ok := <-kresp:
-                if ok == true {
-                } else {
-                    break FLOOP
-                }
-            }
-        }
-    }()
+	go func() {
+	FLOOP:
+		for {
+			select {
+			case _, ok := <-kresp:
+				if ok == true {
+				} else {
+					break FLOOP
+				}
+			}
+		}
+	}()
 
-    rEtcd = &RegEtcd{ctx: ctx,
-        cli:    cli,
-        cancel: cancel,
-        key:    serviceKey}
+	rEtcd = &RegEtcd{ctx: ctx,
+		cli:    cli,
+		cancel: cancel,
+		key:    serviceKey}
 
-    return nil
+	return nil
 }
 
 func UnRegisterEtcd() {
-    //delete
-    rEtcd.cancel()
-    rEtcd.cli.Delete(rEtcd.ctx, rEtcd.key)
+	//delete
+	rEtcd.cancel()
+	rEtcd.cli.Delete(rEtcd.ctx, rEtcd.key)
 }
--- pkg/rpc/rpc.go
+++ pkg/rpc/rpc.go
@@ -1,102 +1,227 @@
 package rpc
 
 import (
-    "context"
-    "fmt"
-    "google.golang.org/grpc/balancer/roundrobin"
-    "google.golang.org/grpc/resolver"
+	"context"
+	"fmt"
+	"net/url"
+	"sync"
 
-    "golang-im/pkg/grpclib/etcdv3"
-    "golang-im/pkg/logger"
-    "golang-im/pkg/pb"
-    "time"
+	"google.golang.org/grpc/balancer/roundrobin"
 
-    "google.golang.org/grpc"
-    "google.golang.org/grpc/keepalive"
+	"golang-im/pkg/grpclib/etcdv3"
+	"golang-im/pkg/logger"
+	"golang-im/pkg/pb"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
+
+	"google.golang.org/grpc/resolver"
 )
 
 const (
-    // grpc options
-    grpcInitialWindowSize     = 1 << 24
-    grpcInitialConnWindowSize = 1 << 24
+	// grpc options
+	grpcInitialWindowSize     = 1 << 24
+	grpcInitialConnWindowSize = 1 << 24
 )
 
 var (
-    // grpc options
-    grpcKeepAliveTime    = time.Duration(10) * time.Second
-    grpcKeepAliveTimeout = time.Duration(3) * time.Second
-    grpcBackoffMaxDelay  = time.Duration(3) * time.Second
-    grpcMaxSendMsgSize   = 1 << 24
-    grpcMaxCallMsgSize   = 1 << 24
+	// grpc options
+	grpcKeepAliveTime    = time.Duration(10) * time.Second
+	grpcKeepAliveTimeout = time.Duration(3) * time.Second
+	grpcBackoffMaxDelay  = time.Duration(3) * time.Second
+	grpcMaxSendMsgSize   = 1 << 24
+	grpcMaxCallMsgSize   = 1 << 24
 
-    // 全局对象 用来访问对应grpc方法
-    LogicIntClient   pb.LogicIntClient
-    ConnectIntClient pb.ConnectIntClient
+	// grpc 服务名称
+	LogicIntSerName   = "logicint_grpc_service"
+	ConnectIntSerName = "connectint_grpc_service"
 
-    // grpc 服务名称
-    LogicIntSerName   = "logicint_grpc_service"
-    ConnectIntSerName = "connectint_grpc_service"
+	Client *client
 )
 
-// InitLogicIntClient connect访问logic不需要知道具体访问哪个节点
-func InitLogicIntClient(schema, etcdaddr string) {
-    rr := etcdv3.NewDiscovery(schema, etcdaddr, LogicIntSerName)
-    resolver.Register(rr) //向resolver/resolver.go 中m变量追加参数和值 m[b.Scheme()] = b
+// 不需要知道具体访问哪个节点
+func newServicenameGrpc(schema, etcdAddr, servicename string) (*grpc.ClientConn, error) {
+	rr := etcdv3.NewDiscovery(schema, etcdAddr, servicename)
+	resolver.Register(rr) //向resolver/resolver.go 中m变量追加参数和值 m[b.Scheme()] = b
 
-    conn, err := grpc.DialContext(
-        context.TODO(),
-        etcdv3.GetPrefix4Unique(schema, LogicIntSerName),
-        []grpc.DialOption{
-            grpc.WithInsecure(), //禁用传输认证，没有这个选项必须设置一种认证方式
-            grpc.WithTimeout(time.Duration(5) * time.Second),
-            grpc.WithInitialWindowSize(grpcInitialWindowSize),
-            grpc.WithInitialConnWindowSize(grpcInitialConnWindowSize),
-            grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(grpcMaxCallMsgSize)),
-            grpc.WithDefaultCallOptions(grpc.MaxCallSendMsgSize(grpcMaxSendMsgSize)),
-            grpc.WithBackoffMaxDelay(grpcBackoffMaxDelay),
-            grpc.WithKeepaliveParams(keepalive.ClientParameters{
-                Time:                grpcKeepAliveTime,
-                Timeout:             grpcKeepAliveTimeout,
-                PermitWithoutStream: true,
-            }),
-            grpc.WithUnaryInterceptor(interceptor), // 一元拦截器，适用于普通rpc连接
-            grpc.WithDefaultServiceConfig(fmt.Sprintf(`{"LoadBalancingPolicy": "%s"}`, roundrobin.Name)),
-        }...)
+	conn, err := grpc.DialContext(
+		context.TODO(),
+		etcdv3.GetPrefix4Unique(schema, servicename),
+		[]grpc.DialOption{
+			grpc.WithInsecure(), //禁用传输认证，没有这个选项必须设置一种认证方式
+			grpc.WithTimeout(time.Duration(5) * time.Second),
+			grpc.WithInitialWindowSize(grpcInitialWindowSize),
+			grpc.WithInitialConnWindowSize(grpcInitialConnWindowSize),
+			grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(grpcMaxCallMsgSize)),
+			grpc.WithDefaultCallOptions(grpc.MaxCallSendMsgSize(grpcMaxSendMsgSize)),
+			grpc.WithBackoffMaxDelay(grpcBackoffMaxDelay),
+			grpc.WithKeepaliveParams(keepalive.ClientParameters{
+				Time:                grpcKeepAliveTime,
+				Timeout:             grpcKeepAliveTimeout,
+				PermitWithoutStream: true,
+			}),
+			grpc.WithUnaryInterceptor(interceptor), // 一元拦截器，适用于普通rpc连接
+			grpc.WithDefaultServiceConfig(fmt.Sprintf(`{"LoadBalancingPolicy": "%s"}`, roundrobin.Name)),
+		}...)
 
-    if err != nil {
-        logger.Sugar.Error(err)
-        panic(err)
-    }
+	if err != nil {
+		logger.Sugar.Error(err)
+		panic(err)
+	}
 
-    LogicIntClient = pb.NewLogicIntClient(conn)
+	return conn, err
 }
 
-// InitConnectIntClient logic 访问 connect 服务最好知道是哪个节点
-func InitConnectIntClient(addr string) (pb.ConnectIntClient, error) {
-    // TODO 判断该addr 是否在服务发现列表里面
-    ctx, cancel := context.WithTimeout(context.Background(), time.Duration(time.Second))
-    defer cancel()
+func newGrpc(addr string) (*grpc.ClientConn, error) {
+	// TODO 判断该addr 是否在服务发现列表里面
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(time.Second))
+	defer cancel()
 
-    conn, err := grpc.DialContext(ctx, addr,
-        []grpc.DialOption{
-            grpc.WithInsecure(),
-            grpc.WithInitialWindowSize(grpcInitialWindowSize),
-            grpc.WithInitialConnWindowSize(grpcInitialConnWindowSize),
-            grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(grpcMaxCallMsgSize)),
-            grpc.WithDefaultCallOptions(grpc.MaxCallSendMsgSize(grpcMaxSendMsgSize)),
-            grpc.WithBackoffMaxDelay(grpcBackoffMaxDelay),
-            grpc.WithKeepaliveParams(keepalive.ClientParameters{
-                Time:                grpcKeepAliveTime,
-                Timeout:             grpcKeepAliveTimeout,
-                PermitWithoutStream: true,
-            }),
-            grpc.WithUnaryInterceptor(interceptor),
-        }...)
+	conn, err := grpc.DialContext(ctx, addr,
+		[]grpc.DialOption{
+			grpc.WithInsecure(),
+			grpc.WithInitialWindowSize(grpcInitialWindowSize),
+			grpc.WithInitialConnWindowSize(grpcInitialConnWindowSize),
+			grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(grpcMaxCallMsgSize)),
+			grpc.WithDefaultCallOptions(grpc.MaxCallSendMsgSize(grpcMaxSendMsgSize)),
+			grpc.WithBackoffMaxDelay(grpcBackoffMaxDelay),
+			grpc.WithKeepaliveParams(keepalive.ClientParameters{
+				Time:                grpcKeepAliveTime,
+				Timeout:             grpcKeepAliveTimeout,
+				PermitWithoutStream: true,
+			}),
+			grpc.WithUnaryInterceptor(interceptor),
+		}...)
 
-    if err != nil {
-        logger.Sugar.Error(err)
-        panic(err)
-    }
+	if err != nil {
+		logger.Sugar.Error(err)
+		panic(err)
+	}
 
-    return pb.NewConnectIntClient(conn), err
+	return conn, err
+}
+
+type client struct {
+	ClientConn map[string]*grpc.ClientConn
+	Lock       sync.RWMutex
+	AllService map[string]string
+	schema     string
+	etcdAddr   string
+}
+
+func NewClient(schema, etcdAddr string) {
+	Client = &client{
+		ClientConn: make(map[string]*grpc.ClientConn),
+		AllService: make(map[string]string),
+		schema:     schema,
+		etcdAddr:   etcdAddr,
+	}
+
+	// 去etcd 检查某服务所有在线节点  更多节点TODO  注意自定义grpc路由不用检查
+	Client.checkNode(schema, etcdAddr, ConnectIntSerName)
+}
+
+// ConnectInt grpc server 服务需要知道是哪个节点
+func (c *client) ConnectInt(addr string) pb.ConnectIntClient {
+	conn := c.getConn(c.schema, c.etcdAddr, addr)
+	if conn == nil {
+		fmt.Errorf("client failed")
+		return nil
+	}
+	return pb.NewConnectIntClient(conn)
+}
+
+// LogicInt grpc server 服务不用知道哪个节点
+func (c *client) LogicInt() pb.LogicIntClient {
+	conn := c.getConn(c.schema, c.etcdAddr, LogicIntSerName)
+	if conn == nil {
+		fmt.Errorf("client failed")
+		return nil
+	}
+	return pb.NewLogicIntClient(conn)
+}
+
+func (c *client) getConn(schema, etcdAddr, servicename string) *grpc.ClientConn {
+	var (
+		r   *grpc.ClientConn
+		err error
+		ok  bool
+		key string
+	)
+	key = etcdv3.GetPrefix4Unique(schema, servicename)
+	//先判断是否已经有了该节点
+	c.Lock.RLock()
+	if r, ok = c.ClientConn[key]; ok {
+		c.Lock.RUnlock()
+		return r
+	}
+	c.Lock.RUnlock()
+
+	c.Lock.Lock()
+	// 判断当前是否 ip:port
+	u, err := url.Parse(key)
+	if u.Port() != "" {
+		// 判断节点是否在线 TODO
+		if _, ok := c.AllService[servicename]; !ok {
+			fmt.Errorf("将要访问的节点  %s 不在线", servicename)
+			return nil
+		}
+		r, err = newGrpc(servicename)
+	} else {
+		r, err = newServicenameGrpc(schema, etcdAddr, servicename)
+	}
+
+	if err != nil {
+		c.Lock.Unlock()
+		return nil
+	}
+
+	c.ClientConn[key] = r
+	c.Lock.Unlock()
+	return r
+}
+
+func (c *client) delConn(schema, servicename string) {
+	c.Lock.Lock()
+	key := etcdv3.GetPrefix4Unique(schema, servicename)
+	delete(c.ClientConn, key)
+	c.Lock.Unlock()
+}
+
+func (c *client) checkNode(schema, etcdAddr, servicename string) {
+	timeoutTicker := 1 * time.Minute
+	ns := etcdv3.GetAllService(schema, etcdAddr, servicename)
+	for k, v := range ns {
+		c.AllService[k] = v
+	}
+
+	// checkTimeout 定时检查在线的connect节点
+	go func() {
+		ticker := time.NewTicker(timeoutTicker)
+		for {
+			select {
+			case <-ticker.C:
+				ns = etcdv3.GetAllService(schema, etcdAddr, servicename)
+				c.handleService(schema, ns)
+			}
+		}
+	}()
+}
+
+// 处理服务节点
+func (c *client) handleService(schema string, ns map[string]string) {
+	// 剔除掉旧的 grpc 连接
+	for k, v := range c.AllService {
+		// 判断旧的节点 在不在最新的etcd 服务节点里
+		if newval, ok := ns[k]; newval == v && !ok {
+			delete(c.AllService, k)
+			c.delConn(schema, k)
+		}
+	}
+
+	// 写入新的节点进来
+	for k, v := range ns {
+		c.AllService[k] = v
+	}
 }
